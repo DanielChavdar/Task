2cfcf37695644b0119a765a62fd2765c75b5a93e
Daniel <daniel.chavdar@icloud.com>
2021-05-11 14:13:47 +0300

    files edit

---
+++ code.js
@@ -0,0 +1,12 @@
+
+let img = document.getElementById('image1');
+
+let canvas = document.getElementById('draw');
+let ctx = canvas.getContext('2d');
+ctx.drawImage(img,20,10);
+
+let img = document.getElementById('image');
+
+let canvas = document.getElementById('draw');
+let ctx = canvas.getContext('2d');
+ctx.drawImage(img,20,10);
