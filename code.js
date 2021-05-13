d9e78136e732931daceec7bf4d04405e53cfc84e
Daniel <daniel.chavdar@icloud.com>
2021-05-13 14:17:43 +0300

    main

--- code.js
+++ code.js
@@ -1,20 +1,20 @@
+let originalCanvas = document.getElementById('originalCanvas');
 
-let example = document.getElementById('example');
+let changedCanvas = document.getElementById('changedCanvas');
 
-    ctx = example.getContext('2d');
-    pic = new Image();
-    pic.src = "image.jpg";
-    pic.onload = function(){
-        ctx.drawImage(pic, 0, 0);
-    };
+ctx = originalCanvas.getContext('2d');
+ctx2 = changedCanvas.getContext('2d');
+
+pic = new Image();
+pic.src = "image.jpg";
+pic.onload = function () {
+    ctx.drawImage(pic, 0, 0);
+};
 
 
-    let example2 = document.getElementById('example2');
-
-    ctx2 = example2.getContext('2d');
-    pic2 = new Image();
-    pic2.src = "image.jpg";
-    pic2.onload = function(){
-        ctx2.drawImage(pic2, 0, 0);
-    };
-example2.style.filter = "brightness(150%)";
+pic2 = new Image();
+pic2.src = "image.jpg";
+pic2.onload = function () {
+    ctx2.drawImage(pic2, 0, 0);
+};
+changedCanvas.style.filter = "brightness(150%)";
