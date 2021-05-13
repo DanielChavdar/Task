034c449ce5c4fb7bf3e73fd9461d7ddf205ca051
Daniel <daniel.chavdar@icloud.com>
2021-05-13 15:00:22 +0300

    main

--- code.js
+++ code.js
@@ -1,20 +1,40 @@
-let originalCanvas = document.getElementById('originalCanvas');
+drawImage('originalCanvas', 'image.jpg', 100);
+drawImage('changedCanvas', 'image.jpg', 150);
 
-let changedCanvas = document.getElementById('changedCanvas');
+function drawImage(canvasName, filename, brightness) {
+    let canvas = document.getElementById(canvasName);
+    let ctx = canvas.getContext('2d');
 
-ctx = originalCanvas.getContext('2d');
-ctx2 = changedCanvas.getContext('2d');
+    let pic = new Image();
+    pic.src = filename;
+    pic.onload = function () {
+        ctx.drawImage(pic, 0, 0);
+    };
 
-pic = new Image();
-pic.src = "image.jpg";
-pic.onload = function () {
-    ctx.drawImage(pic, 0, 0);
+    canvas.style.filter = brightness($ {
+        brightness
+    } % );
 };
 
 
-pic2 = new Image();
-pic2.src = "image.jpg";
-pic2.onload = function () {
-    ctx2.drawImage(pic2, 0, 0);
-};
-changedCanvas.style.filter = "brightness(150%)";
+
+// let originalCanvas = document.getElementById('originalCanvas');
+
+// let changedCanvas = document.getElementById('changedCanvas');
+
+// ctx = originalCanvas.getContext('2d');
+// ctx2 = changedCanvas.getContext('2d');
+
+// pic = new Image();
+// pic.src = "image.jpg";
+// pic.onload = function () {
+//     ctx.drawImage(pic, 0, 0);
+// };
+
+
+// pic2 = new Image();
+// pic2.src = "image.jpg";
+// pic2.onload = function () {
+//     ctx2.drawImage(pic2, 0, 0);
+// };
+// changedCanvas.style.filter = "brightness(150%)";
